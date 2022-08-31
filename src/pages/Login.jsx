cc387dee1202435affa1229c8aae7ad65c2c6496
alijonmrc <alijonmrc99@gmail.com>
2022-08-31 12:06:19 +0500

    login bug

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "../components/loginForm/LoginForm";
+import LoginForm from "../components/LoginForm/LoginForm";
 function Login() {
   return (
     <>
